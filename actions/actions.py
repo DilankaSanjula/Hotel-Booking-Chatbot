90bd9949555aff51d374302f1f14a1862d31e954
DilankaSanjula <dilankasanjula@gmail.com>
2023-09-13 22:30:41 +0530

    Reservation flows integrated with chatgpt

--- actions/actions.py
+++ actions/actions.py
@@ -1,8 +1,11 @@
 from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.events import SlotSet
 import requests
 import json
+from chatgpt.chatgpt import *
+import random
 
 class ActionCheckAvailability(Action):
 
@@ -53,10 +56,88 @@ class ActionReservation(Action):
     def name(self) -> Text:
         return "action_reservation"
 
-    def run(self, dispatcher: CollectingDispatcher,
+    async def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        
-        latest_message = tracker.latest_message.get("text")
 
-        return []
+        entities = tracker.latest_message.get("entities", [])
+
+        room = tracker.get_slot("room")
+        phone = tracker.get_slot("phone")
+        email = tracker.get_slot("email")
+        check_in = tracker.get_slot("check_in")
+
+        last_user_message = tracker.latest_message.get("text")
+    
+        if not tracker.get_slot("conversation"):
+            messages=[
+                {
+                    "role": "system",
+                    "content": "you are a helpful assistant of a Hotel. Need to ask the user about phone,\
+                        email,check in date and room type for reservations. The available room types are standard, deluxe and suite. \
+                        Make it short and human like. For further details contact number is +94119123123"
+                }
+                ]
+            
+            message = {
+                    "role":"user",
+                    "content": str(last_user_message)
+                }
+        
+
+            messages.append(message)
+            print(messages)
+            response = ChatCompletion().chat_completion(messages)
+            bot_response = response['choices'][0]['message']['content']
+            dispatcher.utter_message(bot_response)
+            
+            message_bot = {
+                    "role":"assistant",
+                    "content": str(bot_response)
+                }
+            messages.append(message_bot)
+            print(messages)
+            return [SlotSet("conversation",messages)]
+
+        else:
+            messages = tracker.get_slot("conversation")
+            last_user_message = tracker.latest_message.get("text")
+            message = {
+                "role":"user",
+                "content": str(last_user_message)
+            }
+            messages.append(message)
+
+            response = ChatCompletion().chat_completion(messages)
+            bot_response = response['choices'][0]['message']['content']
+            dispatcher.utter_message(bot_response)
+            
+            message_bot = {
+                    "role":"assistant",
+                    "content": str(bot_response)
+                }
+
+            messages.append(message_bot)
+            entities_list = [room,phone,email,check_in]
+            print(entities_list)
+            if not any(elem is None for elem in entities_list):
+
+                hotel_endpoint = "http://35.168.216.250:7005/availability"
+                json_obj = {"room_type": room}
+                room_count = requests.post(hotel_endpoint, json = json_obj).json()
+                print(room_count)
+                if room_count==0:
+                    dispatcher.utter_message("Sorry we are currently out of rooms\n\n")
+
+                else:
+                    ID = random.randint(1111,9999)
+                    hotel_endpoint_reserve = "http://35.168.216.250:7005/reserve"
+                    json_obj = {"room_type": "standard"}
+                    response_reservation = requests.post(hotel_endpoint_reserve, json = json_obj).json()
+                    dispatcher.utter_message(f"The reservation is made successfully\n\n Reservation ID :{ID}\n Room: {room}\n Date: {check_in}\n Email: {email}\n Contant: {phone}")
+     
+            
+            return [SlotSet("conversation",messages)]
+
+       
+       
--- chatgpt/chatgpt.py
+++ chatgpt/chatgpt.py
@@ -2,8 +2,9 @@ import openai
 import requests
 import re
 import os
+import json
  
-
+api_key = os.environ['api_key']
 openai.api_key = api_key
 url = 'https://api.openai.com/v1/engines/text-davinci-003/completions'
 
@@ -11,27 +12,32 @@ class ChatCompletion:
     def __init__(self):
         print("ChatCompletion")
      
-    def format_message(self,last_message):
-
-        last_message = {
-            "role":"user",
-            "content": str(last_message)
-        }
+    def format_message(self,last_message, role):
+        if role == "user":
+            message = {
+                "role":"user",
+                "content": str(last_message)
+            }
+        if role == "bot":
+            message = {
+                "role":"assistant",
+                "content": str(last_message)
+            }
 
         messages=[
             {
                 "role": "system",
-                "content": "you are a helpful assistant"
+                "content": "you are a helpful assistant of a Hotel. Need to know about phone, email, check in date and room type for reservations. Make is short and human like"
             }
             ]
 
-        messages.append(last_message)
-        print(messages)
+        messages.append(message)
+        #print(messages)
         return messages
 
-    def chat_completion(self, last_message):
+    def chat_completion(self, messages):
 
-        messages = self.format_message(last_message)
+        #messages = self.format_message(last_message, role)
         response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
             messages=messages,
@@ -52,15 +58,26 @@ class ChatEntityExtraction:
 
         response_data = response.json()
         text_data = response_data['choices'][0]['text']
+        print(text_data)
         lines = text_data.split('\n')
         cleaned_entities = {}
+        try:
+            if lines:
+                #print("not empty")
+                for line in lines:
+                    if line != '':
+                        #print(line)
+                        key, value = map(str.strip, line.split(':'))
+                        cleaned_entities[key] = value
+                    
+                return cleaned_entities
+            else:
+                print("empty")
 
-        for line in lines:
-            if line != '':
-                key, value = map(str.strip, line.split(':'))
-                cleaned_entities[key] = value
-            
-        return cleaned_entities
+        except Exception as e:
+            print(e)
+            return {"state": True}
+        
 
     def api_call(self, prompt):
         headers = {
@@ -75,6 +92,7 @@ class ChatEntityExtraction:
             'n': 1
         }
         response = requests.post(url, headers=headers, json=data)
+        #print(response.json())
         if response.status_code == 200:
             entities_response = self.extract_entities(response)
             return entities_response
@@ -109,13 +127,18 @@ class ChatGPTFallback:
 
 
 
-last_message = "hi i need to book standard room on the 4th of september"
+# # # last_message = "hi i need to book standard room on the 4th of september"
 
-text = "hi i need to book a standard room on the 8th of september, name is Dilanka Sanjula,0771473177, dilanka@gmail.com"
+# text =  "Hi can u make reservation on for a deluxe room"
+# text =  text + '.' + "make the reservation under the name Dilanka"
+# text =  text + '.' + "dilankasanjula@gmail.com 07714731977"
+
+text = "Hi can u make reservation on for a deluxe room"
+
 prompt = f'Extract entities such as name, email, phone, dates, room type in this text where dates should be converted to numberical date format: {text}'
 
-text = "what is full board"
-prompt2 = f'Answer this in general hotel context in less than 30 words like answered by a human:{text}'
-#print(ChatCompletion().chat_completion(last_message))
+# # # # # text = "what is full board"
+# # # prompt2 = f'Answer this in general hotel context in less than 30 words like answered by a human:{text}'
+#print(ChatCompletion().chat_completion(last_message))#
 #print(ChatEntityExtraction().api_call(prompt))
-#print(ChatGPTFallback().api_call(prompt2))
+# # # # # #print(ChatGPTFallback().api_call(prompt2))
